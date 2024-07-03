fb3d5f6b45919c286701b94ef17aa00286345ae5
el bestia negra <83923216+catakosmos@users.noreply.github.com>
2024-07-03 19:25:51 -0400

    Update GameWindow.java

--- SourceFiles/GameWindow.java
+++ SourceFiles/GameWindow.java
@@ -5,20 +5,20 @@ import javax.swing.JFrame;
 
 public class GameWindow extends JFrame {
 
+    // Constructor
     public GameWindow() {
         setTitle("Tetris :D");
         setSize(400, 814);
         setResizable(false);
 
-        setLayout(new GridLayout(1, 2));
+        setLayout(new GridLayout(1, 2));                    // establecer layout de la ventana con una fila y 2 columnas 
 
-        // you can adjust timer resolution here. but it's ideal value for this game.
-        GameBoardPanel gameBoard = new GameBoardPanel(this, 400);
+        GameBoardPanel gameBoard = new GameBoardPanel(this, 400);  // panel de tablero con tiempo de resolucion 400
         add(gameBoard);
         gameBoard.start();
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        // operacion por defecto para cerrar la ventana
+        setLocationRelativeTo(null);                           // centra la ventana en la pantalla
+        setVisible(true);                                      // hace visible la ventana
     }
 }
